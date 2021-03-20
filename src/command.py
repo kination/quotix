dfa3e07558ffd5ff849554a14338239a5d827fd1
djKooks <inylove82@gmail.com>
2021-03-20 21:29:31 +0900

    Reformat selected file

--- src/command.py
+++ src/command.py
@@ -3,6 +3,8 @@ import argparse
 from os import listdir
 from os.path import isfile, join
 
+from src.file import replace_quotes
+
 def command(argv):
     # argparse.ArgumentParser()
     parser = argparse.ArgumentParser(description=__doc__, prog='')
@@ -15,9 +17,13 @@ def command(argv):
 
     parser.add_argument('-f', '--files', default='', help='Define files to be re-formatted')
     parser.add_argument('-d', '--directories', default='', help='Define directories to be re-formatted')
+    parser.add_argument('-q', '--quotes', default=None, help='Define quote to be formatted(single/double)')
 
     args = parser.parse_args(argv[1:])
     file_list = set()
+
+    if args.quotes is None:
+        raise SystemExit('Quote rule is not defined')
     
     if args.files is not None:
         file_list.update(args.files.split(','))
@@ -26,4 +32,6 @@ def command(argv):
         path = args.directories
         file_list.update([join(path, f) for f in listdir(path) if isfile(join(path, f))])
 
-    print(file_list)
+    for f in file_list:
+        if f:
+            replace_quotes(f, args.quotes)
